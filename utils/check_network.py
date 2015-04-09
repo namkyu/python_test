f156dc591a32cf000193e2813fcf262cc100b682
unknown <nklee@nklee-notebook.korea.ncsoft.corp>
2015-04-08 23:27:24 -0700

    ACL 체크 util 추가

--- utils/check_network.py
+++ utils/check_network.py
@@ -5,15 +5,25 @@ import socket
 import sys
 
 # check할 대상 서버 리스트 입력 받기
+print("===============================================================================")
 print("please write server list")
-print("and then if you finish writing server list you have to press ctrl+d !!")
+print("and then if you finish writing server list you have to press CTRL+D !!")
+print("===============================================================================")
 data = sys.stdin.read()
+
+# 서버 포트 구분자
+separator_list = [' ', ':']
 
 # 서버 정보 파싱
 server_info_list = data.split("\n")
 for server_info in server_info_list:
     if len(server_info) > 0:
-        server = server_info.split(" ")
+        # 서버 포트 구분자 확인 후 파싱
+        for separator in separator_list:
+            if server_info.find(separator) > -1:
+                server = server_info.split(separator)
+                break
+
         host = server[0]
         ip = int(server[1])
 
